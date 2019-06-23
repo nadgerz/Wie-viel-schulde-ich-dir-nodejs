517156958146a7fd9e96f217a65cb6b0b27f8606
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-06-23 13:57:55 +0200

    for now in one file

---
+++ iou.js
@@ -0,0 +1,6 @@
+
+// faker for 100 unique name
+const all_people = [ "steve", "maurice", "kerstin", "bernhard", "simone", "bob" ]
+console.log( all_people )
+const people = all_people.slice(0, 5)
+console.log( people )
