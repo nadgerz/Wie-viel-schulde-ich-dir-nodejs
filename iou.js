9b60c821550d9421b1d6518085b366b43a3e0228
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-06-23 14:32:02 +0200

    random people

--- iou.js
+++ iou.js
@@ -1,6 +1,94 @@
 
 // faker for 100 unique name
-const all_people = [ "steve", "maurice", "kerstin", "bernhard", "simone", "bob" ]
-console.log( all_people )
-const people = all_people.slice(0, 5)
+let all_people = [
+    'Beck, Glenn',
+    'Becker, Carl',
+    'Beckett, Samuel',
+    'Beddoes, Mick',
+    'Beecher, Henry',
+    'Beethoven, Ludwig',
+    'Begin, Menachem',
+    'Belloc, Hilaire',
+    'Bellow, Saul',
+    'Benchley, Robert',
+    'Benenson, Peter',
+    'Ben-Gurion, David',
+    'Benjamin, Walter',
+    'Benn, Tony',
+    'Bennington, Chester',
+    'Benson, Leana',
+    'Bent, Silas',
+    'Bentsen, Lloyd',
+    'Berger, Ric',
+    'Bergman, Ingmar',
+    'Berio, Luciano',
+    'Berle, Milton',
+    'Berlin, Irving',
+    'Berne, Eric',
+    'Bernhard, Sandra',
+    'Berra, Yogi',
+    'Berry, Halle',
+    'Berry, Wendell',
+    'Bethea, Erin',
+    'Bevan, Aneurin',
+    'Bevel, Ken',
+    'Biden, Joseph',
+    'Bierce, Ambrose',
+    'Biko, Steve',
+    'Billings, Josh',
+    'Biondo, Frank',
+    'Birrell, Augustine',
+    'Black, Elk',
+    'Blair, Robert',
+    'Blair, Tony',
+    'Blake, William',
+];
+
+all_people = [ "steve", "maurice", "kerstin", "bernhard", "simone", "katarina" ]
+// console.log( all_people )
+
+//
+//    Pick a random number of people from the full list, with
+//    a minimum of 2 people.
+//
+const num_people = Math.floor(Math.random() * all_people.length) + 1
+console.log( num_people )
+
+// process.exit(0)
+
+const people = all_people.slice(0, num_people)
 console.log( people )
+process.exit(0)
+
+//
+//    Create a random number of transaction.
+//
+const MAX_TRANSACTIONS = 20
+const num_transactions = Math.floor(Math.random() * MAX_TRANSACTIONS) + 1
+console.log( num_transactions )
+
+//
+//    Generate some random transactions.
+//
+for( i = 0; i<= num_transactions; i++) {
+    console.log( `Loop: ${i}` )
+
+    //
+    //    Pick a random person - they will owe someone something.
+    //
+    const p1 = Math.floor(Math.random() * people.length)
+    console.log( `Person 1: ${p1}`)
+
+    // now get p2, but p2 != p1
+    let p2 = -1
+
+    do {
+	p2 = Math.floor(Math.random() * people.length)
+	console.log( `Try => Person 2: ${p2}`)
+    } while (p2 != p1)
+
+    //
+    //    Now a random $ amount
+    //
+
+}
