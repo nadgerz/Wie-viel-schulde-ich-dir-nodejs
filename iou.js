3820876ede9ab80ff3ab5460cac6d64e1efd7d7f
Stephen A. Ingram <stephen.a.ingram@gmail.com>
2019-06-23 16:48:16 +0200

    wrong logic to end loop

--- iou.js
+++ iou.js
@@ -51,13 +51,12 @@ all_people = [ "steve", "maurice", "kerstin", "bernhard", "simone", "katarina" ]
 //    Pick a random number of people from the full list, with
 //    a minimum of 2 people.
 //
-const num_people = Math.ceil(Math.random() * all_people.length)
-console.log( num_people )
-
-// process.exit(0)
+const num_people = Math.floor(Math.random() * all_people.length) + 2
+// console.log( num_people )
+//process.exit(0)
 
 const people = all_people.slice(0, num_people)
-console.log( people )
+// console.log( people )
 // process.exit(0)
 
 //
@@ -65,37 +64,48 @@ console.log( people )
 //
 const MAX_TRANSACTIONS = 20
 const num_transactions = Math.floor(Math.random() * MAX_TRANSACTIONS) + 1
-console.log( num_transactions )
+console.log( `Number of transactions: ${num_transactions}` )
 
 //
 //    Generate some random transactions.
 //
+const transactions = []
 const MAX_OWED = 20;
 
-for( i = 0; i<= num_transactions; i++) {
-    console.log( `Loop: ${i}` )
+for( i = 1; i<= num_transactions; i++) {
+    // console.log( `Loop: ${i}` )
 
     //
     //    Pick a random person - they will owe someone something.
     //
     const p1 = Math.floor(Math.random() * people.length)
-    console.log( `Person 1: ${p1}`)
+    // console.log( `Person 1: ${p1}`)
 
     // now get p2, but p2 != p1
     let p2 = -1
 
     do {
 	p2 = Math.floor(Math.random() * people.length)
-	console.log( `Try => Person 2: ${p2}`)
-    } while (p2 != p1)
+	// console.log( `Try => Person 2: ${p2}`)
+    } while (p2 === p1)
 
     //
     //    Now a random $ amount
     //
     const owed = Math.floor(Math.random() * MAX_OWED * 100) / 100
-    console.log( `Owed => ${owed}`)
+    // console.log( `Owed => ${owed}`)
+
+    console.log( `Xact ${i}: ${people[p1]} owes ${people[p2]} $${owed}`)
+
+    transactions.concat(
+	{
+	    parasite: p1,
+	    benefactor: p2,
+	    owed
+	}
+    )
 }
 
-
+console.log( transactions )
 
 process.exit( 0 )
